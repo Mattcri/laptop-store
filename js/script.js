23b3aa918aae8b95310303587204afd3b819ddb0
Mcriado <mcriado.cg@gmail.com>
2020-05-09 02:41:52 -0400

    Avance de sección content y scripts

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const btns = document.querySelector('.product__btns')
+
+const btnDisabled = btns.querySelector('.button--disabled')
+const btnActive = btns.querySelector('.button--active')
+
+listener()
+function listener() {
+   btnDisabled.addEventListener('mouseover', changeBtnColor, true)
+   btnDisabled.addEventListener('mouseout', changeBtnColorDisabled, true)
+}
+
+function changeBtnColor() {
+   if(true) {
+      btnDisabled.style.backgroundColor = "#c36cbb"
+      btnDisabled.style.color = "#fff"
+      btnActive.style.backgroundColor = "transparent"
+      btnActive.style.color = "#c36cbb"
+   } 
+}
+
+function changeBtnColorDisabled() {
+   if (true) {
+      btnDisabled.style.backgroundColor = "transparent"
+      btnDisabled.style.color = "#c36cbb"
+      btnActive.style.backgroundColor = "#c36cbb"
+      btnActive.style.color = "#fff"
+   }
+}
+
+console.log(btns)
+console.log(btnDisabled)
+console.log(btnActive)
